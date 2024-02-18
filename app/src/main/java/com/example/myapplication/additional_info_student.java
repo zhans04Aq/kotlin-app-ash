02a94cc5a4f64def84f00384a643bfe91c6f14ce
zhans04Aq <145835709+zhans04Aq@users.noreply.github.com>
2024-02-18 19:39:30 +0500

    second commit

---
+++ app/src/main/java/com/example/myapplication/additional_info_student.java
@@ -0,0 +1,6 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+
+public class additional_info_student extends Activity {
+}
---
+++ app/src/main/java/com/example/myapplication/student_profile.java
@@ -0,0 +1,6 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+
+public class student_profile extends Activity {
+}
